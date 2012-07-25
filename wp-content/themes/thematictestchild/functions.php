3effd70151fc6c7d0b993f3a2f42fbe9197360cf
Montse Lobos <montselobos@gmail.com>
2012-07-25 17:52:35 -0400

    crea child theme de prueba para Thematic

--- wp-content/themes/thematictestchild/functions.php
+++ wp-content/themes/thematictestchild/functions.php
@@ -2,7 +2,8 @@
 /**
  * Custom Child Theme Functions
  *
- * This file's parent directory can be moved to the wp-content/themes directory 
+ *Editado por @montselobos 
+ *This file's parent directory can be moved to the wp-content/themes directory 
  * to allow this Child theme to be activated in the Appearance - Themes section of the WP-Admin.
  *
  * Included are a set of constants that can be defined to customize aspects of Thematic's 
@@ -12,7 +13,7 @@
  * More ideas can be found in the community documentation for Thematic
  * @link http://docs.thematictheme.com
  *
- * @package ThematicSampleChildTheme
+ * @package ThematicTestChild
  * @subpackage ThemeInit
  */
 
@@ -33,13 +34,14 @@
 
 // Adds a home link to your menu
 // http://codex.wordpress.org/Template_Tags/wp_page_menu
-//function childtheme_menu_args($args) {
-//    $args = array(
-//        'show_home' => 'Home',
-//        'sort_column' => 'menu_order',
-//        'menu_class' => 'menu',
-//        'echo' => false
-//    );
-//	return $args;
-//}
-//add_filter('wp_page_menu_args','childtheme_menu_args');
+function childtheme_menu_args($args) {
+    $args = array(
+        'show_home' => 'Home',
+        'sort_column' => 'menu_order',
+        'menu_class' => 'menu',
+        'echo' => false
+   );
+	return $args;
+}
+add_filter('wp_page_menu_args','childtheme_menu_args');
+

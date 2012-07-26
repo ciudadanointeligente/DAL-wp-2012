a2c379252fd576bccccdef5ecc7eb55cc53ea04d
Montse Lobos <montselobos@gmail.com>
2012-07-26 16:54:47 -0400

    Añade las taxonomia theme para posts y guarda los datos

--- wp-content/themes/thematictestchild/functions.php
+++ wp-content/themes/thematictestchild/functions.php
@@ -46,128 +46,100 @@ function childtheme_menu_args($args) {
 add_filter('wp_page_menu_args','childtheme_menu_args');
 
 
-//test paises con dropdown
+//test themes con dropdown on posts
 
-add_action( 'init', 'create_pais_taxonomy', 0 );
+add_action( 'init', 'create_theme_taxonomy', 0 );
  
-function create_pais_taxonomy() {
-	if (!taxonomy_exists('pais')) {
-		register_taxonomy( 'pais', 'page', array( 'hierarchical' => true, 'label' => __('Pais'), 'query_var' => 'pais', 'rewrite' => array( 'slug' => 'pais' ) ) );
+function create_theme_taxonomy() {
+	if (!taxonomy_exists('theme')) {
+		register_taxonomy( 'theme', 'post', array( 'hierarchical' => false, 'label' => __('Theme'), 'query_var' => 'theme', 'rewrite' => array( 'slug' => 'theme' ) ) );
  
-		wp_insert_term('Chile', 'pais');
-		wp_insert_term('Perú', 'pais');
-		wp_insert_term('Argentina', 'pais');
+		wp_insert_term('Beauty', 'theme');
+		wp_insert_term('Dragons', 'theme');
+		wp_insert_term('Halloween', 'theme');
 	}
 }
-
-function add_pais_box() {
-
-	remove_meta_box('paisdiv','page','side');
-
-	add_meta_box('pais_box_ID', __('Pais'), 'your_styling_function', 'page', 'side', 'core');
-
-}	
  
 
-//
-//
-//
-//
-//define funcion para guardar tax
+ function add_theme_box() {
+ 	remove_meta_box('tagsdiv-theme','post','core');
+	add_meta_box('theme_box_ID', __('Theme'), 'your_styling_function', 'post', 'side', 'core');
+}	
+ 
+function add_theme_menus() {
+ 
+	if ( ! is_admin() )
+		return;
+ 
+	add_action('admin_menu', 'add_theme_box');
 
-function save_taxonomy_data($thispage_id) {
+	/* Use the save_post action to save new post data */
+	add_action('save_post', 'save_taxonomy_data');
+}
+ 
+add_theme_menus();
+
+
+// This function gets called in edit-form-advanced.php
+function your_styling_function($post) {
+ 
+	echo '<input type="hidden" name="taxonomy_noncename" id="taxonomy_noncename" value="' . 
+    		wp_create_nonce( 'taxonomy_theme' ) . '" />';
+ 
+ 
+	// Get all theme taxonomy terms
+	$themes = get_terms('theme', 'hide_empty=0'); 
+ 
+?>
+<select name='post_theme' id='post_theme'>
+	<!-- Display themes as options -->
+    <?php 
+        $names = wp_get_object_terms($post->ID, 'theme'); 
+        ?>
+        <option class='theme-option' value='' 
+        <?php if (!count($names)) echo "selected";?>>None</option>
+        <?php
+	foreach ($themes as $theme) {
+		if (!is_wp_error($names) && !empty($names) && !strcmp($theme->slug, $names[0]->slug)) 
+			echo "<option class='theme-option' value='" . $theme->slug . "' selected>" . $theme->name . "</option>\n"; 
+		
+		else
+			echo "<option class='theme-option' value='" . $theme->slug . "'>" . $theme->name . "</option>\n"; 
+	}
+
+   ?>
+</select>    
+<?php
+}
+
+function save_taxonomy_data($post_id) {
 // verify this came from our screen and with proper authorization.
-$myFile = "testFile.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
-$stringData = "Bobby Bopper\n";
-fwrite($fh, $stringData);
-fclose($fh);
-/* 	if ( !wp_verify_nonce( $_POST['taxonomy_noncename'], 'taxonomy_pais' )) {
-    	return $thispage_id;
+ 
+ 	if ( !wp_verify_nonce( $_POST['taxonomy_noncename'], 'taxonomy_theme' )) {
+    	return $post_id;
   	}
  
   	// verify if this is an auto save routine. If it is our form has not been submitted, so we dont want to do anything
   	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
-    	return $thispage_id;
+    	return $post_id;
  
  
   	// Check permissions
   	if ( 'page' == $_POST['post_type'] ) {
-    	if ( !current_user_can( 'edit_page', $thispage_id ) )
-      		return $thispage_id;
+    	if ( !current_user_can( 'edit_page', $post_id ) )
+      		return $post_id;
   	} else {
-    	if ( !current_user_can( 'edit_post', $thispage_id ) )
-      	return $thispage_id;
-  	}*/
+    	if ( !current_user_can( 'edit_post', $post_id ) )
+      	return $post_id;
+  	}
  
   	// OK, we're authenticated: we need to find and save the data
-	$post = get_post($thispage_id);
-	//if (($post->post_type == 'post') || ($post->post_type == 'page')) { 
+	$post = get_post($post_id);
+	if (($post->post_type == 'post') || ($post->post_type == 'page')) { 
            // OR $post->post_type != 'revision'
-           $pais = $_POST['page_pais'];
-
-	   //wp_set_object_terms( $thispage_id, $pais, 'pais' );
-       wp_set_object_terms( $thispage_id, $pais, 'pais' );
-        //}
-
-	return $pais;
+           $theme = $_POST['post_theme'];
+	   wp_set_object_terms( $post_id, $theme, 'theme' );
+        }
+	return $theme;
  
 }
-
-
-// añade menu de paises
-
-function add_pais_menus() {
- 
-	// if ( ! is_admin() )
-	// 	return;
- 
-	add_action('admin_menu', 'add_pais_box');
-	add_action('save_page', 'save_taxonomy_data');
-	//print_r($_POST);
-	//echo $_POST['page_pais'];
-
-}
- 
-add_pais_menus();
-
-
-
-// This function gets called in edit-form-advanced.php
-function your_styling_function($page) {
- 
-	echo '<input type="hidden" name="taxonomy_noncename" id="taxonomy_noncename" value="' . 
-    		wp_create_nonce( 'taxonomy_pais' ) . '" />';
- 
- 
-	// Get all pais taxonomy terms
-	$paises = get_terms('pais', 'hide_empty=0'); 
- 
-?>
-<select name='page_pais' id='page_pais'>
-	<!-- Display paises as options -->
-    <?php 
-        $names = wp_get_object_terms($page->ID, 'pais'); 
-        ?>
-        <option class='pais-option' value='' 
-        <?php if (!count($names)) echo "selected";?>>Ninguno</option>
-        <?php
-	foreach ($paises as $pais) {
-		if (!is_wp_error($names) && !empty($names) && !strcmp($pais->slug, $names[0]->slug)) 
-			echo "<option class='pais-option' value='" . $pais->slug . "' selected>" . $pais->name . "</option>\n"; 
-		else
-			echo "<option class='pais-option' value='" . $pais->slug . "'>" . $pais->name . "</option>\n"; 
-	}
-   ?>
-</select>  
-
-<?php
-
-}
-
-
-
-
-
-
-

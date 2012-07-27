f60ba4274a8f1fb92af31b362cba21ca95bf07ab
Montse Lobos <montselobos@gmail.com>
2012-07-27 13:13:33 -0400

    Adds sample functionality plugin

---
+++ wp-content/plugins/sample-functionality-plugin/sample-functionality-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Your Site's Functionality Plugin
+Description: All of the important functionality of your site belongs in this.
+Version: 0.1
+License: GPL
+Author: Your Name
+Author URI: yoururl
+*/
+
+?>

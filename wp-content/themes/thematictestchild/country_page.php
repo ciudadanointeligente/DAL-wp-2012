e0882364cf56f14912fee8939c42927da7262334
Montse Lobos <montselobos@gmail.com>
2012-07-31 17:02:03 -0400

    crea country page basada en thematic

--- wp-content/themes/thematictestchild/country_page.php
+++ wp-content/themes/thematictestchild/country_page.php
@@ -3,20 +3,84 @@
 Template Name: country_page
 */
 ?>
-<?php get_header(); ?>
+<?php
+ 
+    // calling the header.php
+    get_header();
 
-		<div id="primary">
-			<div id="content" role="main">
+?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+		<div id="container" class="countryPage">
+			thhis is country page, welcome!
+		
+			<?php
 
-					<?php get_template_part( 'content', 'page' ); ?>
+				// filter for manipulating the element that wraps the content 
+				echo apply_filters( 'thematic_open_id_content', '<div id="content">' . "\n" );
+	
+	            // start the loop
+	            while ( have_posts() ) : the_post();
 
-					<?php comments_template( '', true ); ?>
+	        ?>
+    	        
+				<?php
+					echo '<div id="post-' . get_the_ID() . '" ';
+					// Checking for defined constant to enable Thematic's post classes
+					if ( ! ( THEMATIC_COMPATIBLE_POST_CLASS ) ) {
+						post_class();
+						echo '>';
+					} else {
+						echo 'class="';
+						thematic_post_class();
+						echo '">';
+					}
+	                
+	                // creating the post header
+	                 
+				?>
+	                
+	                <h2 class="entry-title country"><?php the_title(); ?></h2>
+					<div class="entry-content">
+	
+						<?php
+	                    	the_content();
+	                    
+	                    	wp_link_pages( "\t\t\t\t\t<div class='page-link'>" . __( 'Pages: ', 'thematic' ), "</div>\n", 'number' );
+	                  
+	                    ?>
 
-				<?php endwhile; // end of the loop. ?>
-
+					</div><!-- .entry-content -->
+					
+				</div><!-- #post -->
+	
+			<?php
+				
+       			// action hook for calling the comments_template
+       			thematic_comments_template();
+        		
+	        	// end loop
+        		endwhile;
+	        
+	        	// calling the sponsors area
+	        	get_template_part( 'local-sponsors' );
+	        ?>
+	
 			</div><!-- #content -->
-		</div><!-- #primary -->
+			
+			<?php 
+				// action hook for placing content below #content
+				thematic_belowcontent(); 
+			?> 
+			
+		</div><!-- #container -->
 
-<?php get_footer(); ?>
+<?php 
+    // action hook for placing content below #container
+    thematic_belowcontainer();
+
+    // calling the standard sidebar 
+    thematic_sidebar();
+    
+    // calling footer.php
+    get_footer();
+?>

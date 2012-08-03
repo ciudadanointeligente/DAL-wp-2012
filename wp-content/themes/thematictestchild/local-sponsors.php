c16eaac1416a7c5122d23ca2dd66b031858d7f64
Montse Lobos <montselobos@gmail.com>
2012-08-03 18:12:47 -0400

    Lista Sponsors en Country Page según término de taxonomía país

--- wp-content/themes/thematictestchild/local-sponsors.php
+++ wp-content/themes/thematictestchild/local-sponsors.php
@@ -2,20 +2,35 @@
 	 
 
 
-	 <ul class="displayEquipo"> 
+	 <ul class="displaySponsorsLocales"> 
         <?php  
         $term = get_term_by( 'slug', get_query_var('term'), get_query_var('taxonomy') );
         query_posts( array( 'post_type' => 'dal_country_sponsor', 'pais'=>$term->name, 'paged' => get_query_var('taxonomy'), 'posts_per_page' => 30, 'orderby' => 'title', 'order' => 'DESC' ) ); ?>
        
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                
-               
+
                     <li>
-                        
-    					<h3 class="archivePostTitle"><a href= <?php the_permalink(); ?> ><?php the_title(); ?></a></h3>
+                        <?php 
+
+							if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+								?>
+							<a href="<?php the_permalink(); ?>"> <?php  the_post_thumbnail(); ?></a>
+							
+						<?php 
+								}  
+							else
+								{
+						?>
+
+							<a href=" <?php the_permalink(); ?> "> <?php the_title(); ?> </a>
+						
+						<?php
+
+							}	
+						?>
                 	
                     </li>
-                
+                  
                       
              <?php endwhile; else: ?>
             <?php endif; ?>
@@ -26,6 +41,3 @@
 
 </div>
 
- 
-
- 

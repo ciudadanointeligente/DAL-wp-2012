004e7247dfbd9c196691b6aaa629bd24fb6f4d7a
Montse Lobos <montselobos@gmail.com>
2012-09-03 17:24:53 -0400

    crea metabox para el CPT "organizer"

--- wp-content/plugins/dal-functionality/includes/metabox_code/functions/organizer_metabox.php
+++ wp-content/plugins/dal-functionality/includes/metabox_code/functions/organizer_metabox.php
@@ -1,7 +1,5 @@
 <?php 
-    $prefix = 'tz_';
-
-    if(is_admin()) {
+if(is_admin()) {
         wp_enqueue_script('custom-js', get_bloginfo('wpurl').'/wp-content/plugins/dal-functionality/includes/metabox_code/js/custom-js.js');
         wp_enqueue_style('jquery-ui-custom',get_bloginfo('wpurl').'/wp-content/plugins/dal-functionality/includes/metabox_code/css/jquery-ui-custom.css');
         wp_enqueue_script('media-upload');
@@ -11,144 +9,125 @@
         wp_enqueue_style('thickbox');
     }
 
+ // Add the Meta Box
+function add_org_box() {
+    add_meta_box(
+        'organizer_m_box', // $id
+        'Logo y URL', // $title 
+        'show_org_m_box', // $callback
+        'dal_organizers', // $page
+        'normal', // $context
+        'core'); // $priority
+}
+add_action('add_meta_boxes', 'add_org_box');
 
-    $organizers_fields = array(
-         'id' => 'Organizers',
-         'title' => 'Organizers',
-         'page' => 'portfolio',
-         'context' => 'normal',
-         'priority' => 'high',
-         'fields' => array(
-            array(
-                'name' => 'Add ',
-                'desc' => 'This meta box is under development',
-                'id' => $prefix . 'organizers',
-                'type' => 'text',
-                'std' => ''
-            ),   
-            array(
-            'label' => 'Logo organizer',
-            'desc'  => 'Agrega el logo de los organizadores locales',
-            'id'    => $prefix.'orglogo',
-            'type'  => 'image'
-            ),                     
-        )
-    );
-
-    /*-------------------------------------*/
-    /* Add Meta Box to CPT screen 
-    /*-------------------------------------*/
-
-    function tz_add_box() {
-        global $organizers_fields;
-
-        add_meta_box($organizers_fields['id'], $organizers_fields['title'], 'tz_show_box_organizers', $organizers_fields['page'], $organizers_fields['context'], $organizers_fields['priority']);
-
-
-    }
-
- 
-
- add_action('admin_menu', 'tz_add_box');
-
-    /*------------------------------------------*/
-    /* Callback function/show fields in meta box
-    This is taken directly from: http://wordpress.stackexchange.com/questions/19838/create-more-meta-boxes-as-needed
-    /*------------------------------------------*/
-
-
-    function tz_show_box_organizers() {
-        global $organizers_fields, $post;
-        // Use nonce for verification
-        echo '<input type="hidden" name="tz_meta_box_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
-        ?>
-
-
-
-        <div id="meta_inner">
-        <?php
-        //get the saved meta as an array
-       
-        $orgs = get_post_meta( $post->ID, 'orgs', true );
-        $c = 0;
-        if ( count( $orgs ) > 0 ) {
-            foreach ( (array)$orgs as $org ) {
-                print_r($org);
-                if ( isset( $org['title'] ) || isset( $org['orglink'] ) || isset( $org['orglogo'] ) ) {
-
-                    $singleorg = '<p>Org Name<input type="text" name="orgs[%1$s][title]" value="%2$s" />';
-                    $singleorg .= ' -- link organizer : <input type="text" name="orgs[%1$s][orglink]" value="%3$s" />';
-                    $image = get_template_directory_uri().'/img/image.png';  
-                    $singleorg .= '<span class="custom_default_image" style="display:none">'.$image.'</span>';
-                    if ($org['orglogo']) { $image = wp_get_attachment_image_src($org['orglogo'], 'medium'); $image = $image[0]; }    
-
-                    $singleorg .= '<input name="orgs[%1$s][orglogo]" type="hidden" class="custom_upload_image" value="%5$s" />
-                                    <img src="'.$image.'" class="custom_preview_image" alt="" />
-                                    <br />
-                                    <input class="custom_upload_image_button button" type="button" value="Choose Image" />
-                                    <small>&nbsp;<a href="#" class="custom_clear_image_button">Remove Image</a></small>';
-                    $singleorg .='<br clear="all" /><span class="description">'.$org['desc'].'</span>';
-                    $singleorg .= '<span class="remove">%4$s</span></p>';
-                    printf( $singleorg, $c, $org['title'], $org['orglink'], __( 'Remove org' ), $org['orglogo'] );
-                    $c++;
-                }
-            }
-        }
-
-
-
-        ?>
-        <div class="custom_upload_image_button button"> hola</div>
-        <span id="here"></span>
-        <span class="add"><?php _e('Add organizer'); ?></span>
-        <script>
-        var $ =jQuery.noConflict();
-        $(document).ready(function() {
-        var count = <?php echo $c; ?>;
-        $(".add").click(function() {
-        count = count + 1;
-        $('#here').before($('<p> Org Name <input type="text" name="orgs['+count+'][title]" value="" /> -- Link: <input type="url" name="orgs['+count+'][orglink]" value="" /> <span class="custom_default_image" style="display:none">--</span> <input name="orgs['+count+'][orglogo]" type="hidden" class="custom_upload_image" value=""/> <img src="" class="custom_preview_image" alt="" /><br /><input class="custom_upload_image_button button" type="button" onclick="imageLoader()" value="Choose Image"/><span class="remove">Remove organization link</span></p>'));
-        return false;
-        });
-        $(".remove").live('click', function() {
-        $(this).parent().remove();
-        });
-        });
-        </script>
-        </div>
-        <?php
-
-    }
-
-/*-------------------------------------*/
-/* Save data when post is edited
-/*-------------------------------------*/
-
-
-function tz_save_data($post_id) {
-    global $organizers_fields, $post;
-    // verify nonce
-    if (!wp_verify_nonce($_POST['tz_meta_box_nonce'], basename(__FILE__))) {
-        return $post_id;
-    }
-    // check autosave
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return $post_id;
-    }
-    // check permissions
-    if ('page' == $_POST['post_type']) {
-        if (!current_user_can('edit_page', $post_id)) {
-            return $post_id;
-        }
-    } elseif (!current_user_can('edit_post', $post_id)) {
-        return $post_id;
-    }
-
-
- $orgs = $_POST['orgs'];
-    update_post_meta($post_id,'orgs',$orgs);
-
+$prefix = 'org_';
+$org_meta_fields = array(
+    
+    array(
+        'label' => 'logo',
+        'desc'  => 'logo de la organizacion',
+        'id'    => $prefix.'logo',
+        'type'  => 'image'
+    ),
+     array(
+        'label' => 'link organización',
+        'desc'  => 'url organización, debes anteponer http://',
+        'id'    => $prefix.'link',
+        'type'  => 'link'
+    ),
+     array(
+        'label' => 'Descripción organización',
+        'desc'  => '',
+        'id'    => $prefix.'desc_org',
+        'type'  => 'textarea'
+    ),
+    
+);
+// The Callback
+function show_org_m_box() {
+    global $org_meta_fields, $post;
+    // Use nonce for verification
+    echo '<input type="hidden" name="country_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
+    
+    // Begin the field table and loop
+    echo '<table class="form-table">';
+    foreach ($org_meta_fields as $field) {
+        // get value of this field if it exists for this post
+        $meta = get_post_meta($post->ID, $field['id'], true);
+        // begin a table row with
+        echo '<tr>
+                <th><label for="'.$field['id'].'">'.$field['label'].'</label></th>
+                <td>';
+                switch($field['type']) {
+                    // image
+                    case 'image':
+                        $image = get_template_directory_uri().'/img/image.png'; 
+                        echo '<span class="custom_default_image" style="display:none">'.$image.'</span>';
+                        if ($meta) { $image = wp_get_attachment_image_src($meta, 'medium'); $image = $image[0]; }               
+                        echo    '<input name="'.$field['id'].'" type="hidden" class="custom_upload_image" value="'.$meta.'" />
+                                    <img src="'.$image.'" class="custom_preview_image" alt="" /><br />
+                                        <input class="custom_upload_image_button button" type="button" value="Elegir imagen" />
+                                        <small>&nbsp;<a href="#" class="custom_clear_image_button">Remove Image</a></small>
+                                        <br clear="all" /><span class="description">'.$field['desc'].'</span>';
+                    break;
+                    //link
+                    case 'link':
+                        if ( !preg_match( "/http(s?):\/\//", $meta )) {
+                            $errors = 'Url not valid';
+                            $meta = 'http://';
+                          } 
+                        echo '<input type="url" name="'.$field['id'].'" id="'.$field['id'].'" value="'.$meta.'" size="30"  placeholder="http://url.com"/>
+                                <br /><span class="description">'.$field['desc'].'</span>';
+                    break;
+                    // textarea
+                    case 'textarea':
+                        echo '<textarea name="'.$field['id'].'" id="'.$field['id'].'" cols="60" rows="4">'.$meta.'</textarea>
+                                <br /><span class="description">'.$field['desc'].'</span>';
+                    break;
+                    } //end switch
+        echo '</td></tr>';
+    } // end foreach
+    echo '</table>'; // end table
 }
 
-add_action('save_post', 'tz_save_data');
+// Save the Data
+function save_org_meta($post_id) {
+    global $org_meta_fields;
+    
+    // verify nonce
+    if (!wp_verify_nonce($_POST['country_meta_box_nonce'], basename(__FILE__))) 
+        return $post_id;
+    // check autosave
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+        return $post_id;
+    // check permissions
+    if ('page' == $_POST['post_type']) {
+        if (!current_user_can('edit_page', $post_id))
+            return $post_id;
+        } elseif (!current_user_can('edit_post', $post_id)) {
+            return $post_id;
+    }
+    
+    // loop through fields and save the data
+    foreach ($org_meta_fields as $field) {
+        if($field['type'] == 'tax_select') continue;
+        $old = get_post_meta($post_id, $field['id'], true);
+        $new = $_POST[$field['id']];
+        if ($new && $new != $old) {
+            update_post_meta($post_id, $field['id'], $new);
+        } elseif ('' == $new && $old) {
+            delete_post_meta($post_id, $field['id'], $old);
+        }
+    } // end foreach
+    
+    
+// save taxonomies
+    $post = get_post($post_id);
+    $category = $_POST['category'];
+    wp_set_object_terms( $post_id, $category, 'category' );
+    
+}
+add_action('save_post', 'save_org_meta');
+
 ?>

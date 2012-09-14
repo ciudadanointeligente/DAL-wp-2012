805728e8569cf11bd5a2c5d941acd64bf05d2a54
Montse Lobos <montselobos@gmail.com>
2012-09-14 17:37:56 -0400

    Estilo home dal2012
    responsive

--- wp-content/themes/bootstrapwp-87/page-home-dal2012.php
+++ wp-content/themes/bootstrapwp-87/page-home-dal2012.php
@@ -13,60 +13,63 @@
 get_header(); ?>
 <div class="container">
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-  <header class="jumbotron masthead nolist">
-    <div class="inner">
-      <div class="row">
-        <div class="span7">
-          <div class="row">
-            <div class="span5">
-               <?php
-                if ( function_exists('dynamic_sidebar')) dynamic_sidebar("hero-left");
-                ?>
+  <header class="jumbotron nolist">
+  
+       
+            <div class="row">
+              <div class="blackground span">
+                <div class="span5">
+                  <?php
+                    if ( function_exists('dynamic_sidebar')) dynamic_sidebar("hero-left");
+                  ?>
+         
+                  <?php
+                      if ( function_exists('dynamic_sidebar'));
+                      echo '<div class="row">'.dynamic_sidebar("hero-right").'</div>';
+                  ?>
+                </div>
+                <div class="span5">
+                  <?php wp_nav_menu( array( 'theme_location' => 'hero-menu', 'container_class' => 'hero-menu' ) ); ?> 
+                </div><!--/row-->
+             
+               
+            </div>  
+         <div class="blackbottom span"> 
+              </div>
+                
+</header>
+
+    <div class="row wrapper">
+       <div class="row">
+        <div class="left-box span4">
+          <div class="box-top"></div>
+            <div class="box-contentwrap">
+             <?php
+              if ( function_exists('dynamic_sidebar')) dynamic_sidebar("home-left");
+              ?>
             </div>
-            <div class="span2">
-              a
-            </div>    
-          </div>  
-        </div><!--/span4-->
-
-        <div class="span5">
-
-          <?php
-              if ( function_exists('dynamic_sidebar'));
-              echo '<div class="row">'.dynamic_sidebar("hero-right").'</div>';
-          ?>
-          <div class="row">
-           <?php wp_nav_menu( array( 'theme_location' => 'hero-menu', 'container_class' => 'hero-menu' ) ); ?>
-         </div><!--/row-->
-
-        </div><!--/span4-->
-      </div><!--/row-->
-      <hr class="soften">
+        </div>  
+        <div class="center-box span4">
+          <div class="box-top"></div>
+            <div class="box-contentwrap">
+              <?php
+              if ( function_exists('dynamic_sidebar')) dynamic_sidebar("home-left");
+              ?>
+            </div>
+        </div>    
+        <div class="right-box span4">
+          <div class="box-top"></div>
+            <div class="box-contentwrap">
+             <?php
+              if ( function_exists('dynamic_sidebar')) dynamic_sidebar("home-right");
+              ?>
+            </div>
+        </div>   
+      </div>
+    </div>  
+       <hr class="soften">
       <?php the_content();?>
+      <?php endwhile; endif; ?>
       <hr class="soften">
-    </div>
-
-
-  </header>
-<?php endwhile; endif; ?>
-
-<div class="marketing">
-  <div class="row">
-    <div class="span4">a
-      <?php
-      if ( function_exists('dynamic_sidebar')) dynamic_sidebar("home-left");
-      ?>
-    </div>
-    <div class="span4">b
-      <?php
-      if ( function_exists('dynamic_sidebar')) dynamic_sidebar("home-middle");
-      ?>
-    </div>
-    <div class="span4">c
-      <?php
-      if ( function_exists('dynamic_sidebar')) dynamic_sidebar("home-right");
-      ?>
-    </div>
-  </div>
-</div><!-- /.marketing -->
+  </div>  
 <?php get_footer();?>
